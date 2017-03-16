e46c708e149b8a07afddfe36583f187a16b47ee1
Ali <ali@iMac.local>
2017-03-16 22:22:26 +0000

    no message

--- myTableViewProject/ViewController.swift
+++ myTableViewProject/ViewController.swift
@@ -8,19 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var maintTableView: UITableView!
+    
+    var emojis = ["😙","🌝","🐈","😎","😍","🏕","🛶","🏛"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib...
+       
+        maintTableView.dataSource = self
+        maintTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
         
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
